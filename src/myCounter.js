6f2dc008480824cd99ee516e612e9ab99995ffd8
José Victor Macedo Veloso <vitovls37@gmail.com>
2021-07-27 19:40:49 -0300

    Correções lint

--- src/myCounter.js
+++ src/myCounter.js
@@ -27,5 +27,4 @@ const myCounter = () => {
   }
   return myArray;
 };
-console.log(myCounter())
 module.exports = myCounter;
--- tests/myCounter.spec.js
+++ tests/myCounter.spec.js
@@ -1,5 +1,5 @@
-const assert = require('assert');
-const myCounter = require('../src/myCounter');
+const assert = require("assert");
+const myCounter = require("../src/myCounter");
 
 /*
   A função myCounter possui dois loops aninhados que inserem valores dentro de um array. Como podemos perceber, eles vão adicionando valores ao array até sua condição de parada. Corrija o código abaixo para que a função retorne o array correto.
@@ -13,8 +13,8 @@ const myCounter = require('../src/myCounter');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('8 - Implemente a função `myCounter`', () => {
-  it('Verifica se a função `myCounter` funciona como o esperado', () => {
+describe("8 - Implemente a função `myCounter`", () => {
+  it("Verifica se a função `myCounter` funciona como o esperado", () => {
     const expectedOutput = [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
     assert.deepStrictEqual(myCounter(), expectedOutput);
   });
