7e41583f7c60f663811c31ca02005e8e7fe3c355
José Victor Macedo Veloso <vitovls37@gmail.com>
2021-07-27 15:58:15 -0300

    Requisito 1 - Implementando a função average

--- src/average.js
+++ src/average.js
@@ -12,8 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // add your implementation here
+const average = (myArray) => {
+  let number = 0;
+  let numberInt = 0;
+  if (myArray.length === 0) {
+    return undefined;
+  }
+  for (let i = 0; i < myArray.length; i += 1) {
+    if (typeof myArray[i] !== 'number') {
+      return undefined;
+    }
+    number += myArray[i];
+  }
+  numberInt = number / myArray.length;
+  return Math.round(numberInt);
 };
 
 module.exports = average;
