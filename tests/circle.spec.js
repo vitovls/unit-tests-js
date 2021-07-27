94229691a0803ff8be280a160d8c51022e06e8a1
José Victor Macedo Veloso <vitovls37@gmail.com>
2021-07-27 18:41:41 -0300

    Requisito 4 - Implementar os casos de teste para a função circle.

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -34,28 +34,16 @@ describe("4 - Implemente os casos de teste para a função `circle`", () => {
   it("Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados", () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-    assert.ok(typeof circle(50) === "object");
+    assert.strictEqual(typeof circle(50), "object");
     // Teste se o objeto retornado tem 3 entradas.
-
+    assert.strictEqual(Object.keys(circle(3)).length, 3)
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    assert.deepStrictEqual(
-      circle(2),
-      { "area": 12.56, "circumference": 12.56, "radius": 2 },
-      "Raio errado!"
-    );
+    assert.strictEqual(circle(2).circumference, 12.56)
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.deepStrictEqual(
-      circle(3),
-      { "area": 28.26, "circumference": 18.84, "radius": 3 },
-      "Raio errado!"
-    );
+    assert.strictEqual(circle(3).area, 28.259999999999998)
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.deepStrictEqual(
-      circle(3),
-      { "area": 28.26, "circumference": 18.84, "radius": 3 },
-      "Raio errado!"
-    );
+    assert.deepStrictEqual(circle(3), {"area": 28.259999999999998, "circumference": 18.84, "radius": 3})
   });
 });
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,5 +1,5 @@
-const assert = require('assert');
-const productDetails = require('../src/productDetails');
+const assert = require("assert");
+const productDetails = require("../src/productDetails");
 
 /*
   Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
@@ -28,13 +28,15 @@ const productDetails = require('../src/productDetails');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+describe("6 - Implemente os casos de teste para a função `productDetails`", () => {
+  it("Verifica se a função `productDetails` tem o comportamento esperado", () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails()), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+
     // Teste que os dois objetos são diferentes entre si.
     // Teste que os dois productIds terminam com 123.
   });
